c417389628bf015b58b1a9dac9f368eda4ab271b
Ebloin <boninsegna.1705655@studenti.uniroma1.it>
2018-11-27 16:15:23 +0100

    Testing

--- visualizer.py
+++ visualizer.py
@@ -1,9 +1,7 @@
 from collections import OrderedDict
-import pylab as pl
 import numpy as np
 import cv2
-import time
-import visual
+import vpython
 
 #PACKET EXAMPLE
 #{seq:01094, x:0.000, y:0.000, t:0.000, tvm:0.000, tvd:0.600, tva:0.060, rvm:0.000, rvd:0.100, rva:0.100}
@@ -28,15 +26,15 @@ for l in file:
         images[int(linea[0])]= "./"+linea[2]
 
 back= np.zeros((512,512,3), np.uint8)
-back = cv2.line(back,(0,0),(511,511),(255,0,0),5)
-
 cv2.namedWindow('frames', cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow('pos', cv2.WINDOW_AUTOSIZE)
 for k in drive_packets:
+    back = cv2.circle(back, ())
     data = parsepacket(drive_packets[k])
     print("POSIZIONE:\tX: "+data['x']+"\tY: "+ data['y']+ "\n")
     if(images.has_key(k)):
-        print images[k]
         img = cv2.imread(images[k])
         cv2.imshow('frames', img)
+        cv2.imshow('pos', back)
         cv2.waitKey(0)
 cv2.destroyAllWindows()
